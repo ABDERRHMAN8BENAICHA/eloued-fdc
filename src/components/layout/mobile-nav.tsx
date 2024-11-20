09019520e1077b3c6aa87ea7871b719ee8114d36
Mohamed-lifa7 <mohamedlifa7@gmail.com>
2024-11-20 17:21:44 +0100

    Clean code more

--- src/components/layout/mobile-nav.tsx
+++ src/components/layout/mobile-nav.tsx
@@ -6,7 +6,6 @@ import { useRouter } from "next/navigation";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
 import { cn } from "@/lib/utils";
-import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
